bd2d1e6c6a74cbebaf92abb9a53c4880b939339b
Liam McCabe <lmccabe267@gmail.com>
2022-06-28 17:27:21 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+/*
+ * 1.import
+ * 2.load and register driver
+ * 3.create connection
+ * 4.create a statement
+ * 5.execute the query
+ * 6.process the results
+ * 7.close
+ */
+
+public class Main {
+	public static void main(String[]args) {
+		
+	}
+}
