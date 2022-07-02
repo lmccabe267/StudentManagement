1c734158f7591ceca8841b9ad100227e1ac686c9
Liam McCabe <lmccabe267@gmail.com>
2022-07-01 21:13:20 -0700

    created class to handle database management

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,14 @@
-import java.sql.*;
-
-/*
- * 1.import --> java.sql
- * 2.load and register driver --> com.mysql.jdbc.Driver
- * 3.create connection --> Connection
- * 4.create a statement --> Statement
- * 5.execute the query ->
- * 6.process the results ->
- * 7.close
- */
-
-
 
 public class Main {
+	
 	public static void main(String[]args) throws Exception{
+		DBManager dbm = new DBManager("jdbc:mysql://localhost:3306/school", "root", args[0]);
 		
-		String url = "jdbc:mysql://localhost:3306/school";
-		String uname = "root";
-		String pass = args[0];
-		String query = "select name from students where student_id=2";
+		dbm.printResultSet(dbm.query("SELECT * FROM students"));
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection(url, uname, pass);
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(query);
+		dbm.stopConnection();
 		
-		rs.next();
-		String name = rs.getString("name");
-		
-		System.out.println(name);
-		
-		st.close();
-		con.close();
 	}
+	
+	
 }
